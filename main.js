6b94cabf149c095498934da424d85dc88b99dcdc
Nikola Milun <nikola.milun2508@gmail.com>
2022-02-22 08:41:12 +0100

    Added all animations, fixed main.js problem
    Added animations through animatecss and wow.js, fixed the script problem + some minor changes in the comments and design.

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-new WOW.init();
+//General
+new WOW().init();
 const modalimg = document.querySelector('#modalimg');
 const navbarMenu = document.querySelector('#mainmenu');
 const navLinks = document.querySelectorAll('.nav-link');
+//Gallery modal image changes
 function otvoriModal(br){
-    modalimg.src = "images/gal" + br + ".jpg";
+    modalimg.src = `images/gal${br}.jpg`;
 }
+//Some navbar smoothing changes
 navLinks.forEach(item => item.addEventListener('click', function() {
     navbarMenu.classList.remove('show');
-    navbarMenu.classList.add('hide');
 }))
