c5024400b43d18a0770f23003ac10ebcb1f128e7
Nikola Milun <nikola.milun2508@gmail.com>
2022-02-27 13:36:45 +0100

    Tried to make a page scroll progbar
    Everything is fine I just can't find out how to make it go all the way up.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ new WOW().init();
 const modalimg = document.querySelector('#modalimg');
 const navbarMenu = document.querySelector('#mainmenu');
 const navLinks = document.querySelectorAll('.nav-link');
+const progBar = document.querySelector('#pbar');
 //Gallery modal image changes
 function otvoriModal(br){
     modalimg.src = `images/gal${br}.jpg`;
@@ -11,3 +12,14 @@ function otvoriModal(br){
 navLinks.forEach(item => item.addEventListener('click', function() {
     navbarMenu.classList.remove('show');
 }))
+function getPerc(){
+    let rez = Math.round(window.scrollY / (document.body.offsetHeight - window.innerHeight) * 100);
+    if(rez > 100)
+        rez = 100;
+    return rez;
+}
+window.addEventListener('scroll', function(){
+    let off = this.scrollY;
+    progBar.ariaValueNow = `${getPerc()}`;
+    progBar.style.width = `${getPerc()}%`;
+})
